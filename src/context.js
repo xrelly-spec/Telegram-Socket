c027f87daaad53e4b16609c4d0f2889c3d3f8d65
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-03 02:04:44 +0700

    Update context.js

--- src/context.js
+++ src/context.js
@@ -1,11 +1,29 @@
-function buildContext(api, msg) {
-  msg.reply = (text, options = {}) =>
-    api.sendMessage(msg.chat.id, text, {
-      reply_to_message_id: msg.message_id,
-      ...options
-    })
+function buildContext(api, msg, socket) {
+  const text = msg.text || ""
 
-  return msg
+  const ctx = {
+    api,
+    socket,
+
+    message: msg,
+
+    chat: msg.chat,
+    from: msg.from,
+    text,
+    args: text.split(" ").slice(1),
+    isGroup: msg.chat?.type !== "private",
+
+    reply(text, options = {}) {
+      return api.call("sendMessage", {
+        chat_id: msg.chat.id,
+        text,
+        reply_to_message_id: msg.message_id,
+        ...options
+      })
+    }
+  }
+
+  return ctx
 }
 
 module.exports = buildContext
