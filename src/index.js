6386afd1f59cb9365f46fc8e6f8c66a0ceb092ca
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-03 01:45:55 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,22 +7,12 @@ function makeTelegramSocket(opts) {
 
 const obfuscator = new ObfuscatorManager()
 
-obfuscator.register(
-  "jsconfuser",
-  require("./obfuscator/jsconfuser")
-)
-
-obfuscator.register(
-  "javascript-obfuscator",
-  require("./obfuscator/javascriptObf")
-)
-
-obfuscator.register(
-  "terser",
-  require("./obfuscator/terser")
-)
+obfuscator.register("jsconfuser", require("./obfuscator/jsconfuser"))
+obfuscator.register("javascript-obfuscator", require("./obfuscator/javascriptObf"))
+obfuscator.register("terser", require("./obfuscator/terser"))
 
 module.exports = {
   makeTelegramSocket,
-  obfuscator
+  obfuscator,
+  ObfuscatorManager
 }
