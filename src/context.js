1354ef6300d302dfaf206466676baa43a15aa963
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-03 03:28:24 +0700

    Update context.js

--- src/context.js
+++ src/context.js
@@ -5,6 +5,21 @@ function buildContext(api, msg) {
       ...options
     })
 
+  msg.edit = (text, options = {}) =>
+    api.editMessageText(msg.chat.id, msg.message_id, text, options)
+
+  msg.sendPhoto = (photo, caption = "", options = {}) =>
+    api.sendPhoto(msg.chat.id, photo, caption, options)
+  
+  msg.sendVideo = (video, caption = "", options = {}) =>
+    api.sendVideo(msg.chat.id, video, caption, options)
+
+  msg.sendDocument = (document, caption = "", options = {}) =>
+    api.sendDocument(msg.chat.id, document, caption, options)
+
+  msg.sendContact = (phone, firstName, options = {}) =>
+    api.sendContact(msg.chat.id, phone, firstName, options)
+
   return msg
 }
 
