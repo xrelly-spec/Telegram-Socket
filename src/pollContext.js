b66cf51ff225fbbb37dff4de4aa9ac7dedd59fb1
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-04 02:01:42 +0700

    Update pollContext.js

--- src/pollContext.js
+++ src/pollContext.js
@@ -1,23 +1,19 @@
 function buildPollContext(api, pollAnswer) {
-  const userId = pollAnswer.user.id
+  const user = pollAnswer.user || {}
+  const userId = user.id
 
   return {
     api,
-
     pollAnswer,
-    user: pollAnswer.user,
-    optionIds: pollAnswer.option_ids,
 
+    user,
+    option_ids: Array.isArray(pollAnswer.option_ids)
+      ? pollAnswer.option_ids
+      : [],
+    
     reply(text, options = {}) {
+      if (!userId) return
       return api.sendMessage(userId, text, options)
-    },
-
-    sendLocation(lat, lon, options = {}) {
-      return api.sendLocation(userId, lat, lon, options)
-    },
-
-    sendVenue(lat, lon, title, address = "", options = {}) {
-      return api.sendVenue(userId, lat, lon, title, address, options)
     }
   }
 }
