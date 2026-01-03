4e121af39372e47a699d592c1f9dd5a5b663ccc4
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-04 00:42:19 +0700

    Update pollContext.js

--- src/pollContext.js
+++ src/pollContext.js
@@ -1,15 +1,37 @@
 function buildPollContext(api, pollAnswer) {
-  const userId = pollAnswer.user.id
+  const userId = pollAnswer.user?.id
 
-  return {
+  if (!userId)
+    throw new Error("PollAnswer without user id")
+
+  const ctx = {
     api,
-    pollAnswer,
 
+    pollAnswer,
     user: pollAnswer.user,
     optionIds: pollAnswer.option_ids,
 
     reply(text, options = {}) {
       return api.sendMessage(userId, text, options)
+    },
+
+    sendPhoto(photo, caption = "", options = {}) {
+      return api.sendPhoto(userId, photo, caption, options)
+    },
+
+    sendVideo(video, caption = "", options = {}) {
+      return api.sendVideo(userId, video, caption, options)
+    },
+
+    sendDocument(document, caption = "", options = {}) {
+      return api.sendDocument(userId, document, caption, options)
+    },
+
+    sendButtons(text, buttons, options = {}) {
+      return api.sendMessage(userId, text, {
+        reply_markup: { inline_keyboard: buttons },
+        ...options
+      })
     },
 
     sendVenue(lat, lon, title, address = "", options = {}) {
@@ -23,6 +45,8 @@ function buildPollContext(api, pollAnswer) {
       })
     }
   }
+
+  return ctx
 }
 
 module.exports = buildPollContext
