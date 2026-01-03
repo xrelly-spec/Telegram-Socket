2e7d226fc194588f9c81221d831ff62b7e9b0449
xrelly-spec <rendirajabpratama@gmail.com>
2026-01-03 15:07:31 +0700

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,38 @@
 function parseUpdate(update) {
-  return {
+  const parsed = {
     updateId: update.update_id,
-    message: update.message || null,
-    callback: update.callback_query || null,
+    type: null,
+    message: null,
+    callback: null,
+    poll: null,
     raw: update
   }
+
+  if (update.message) {
+    parsed.type = "message"
+    parsed.message = update.message
+    return parsed
+  }
+
+  if (update.callback_query) {
+    parsed.type = "callback_query"
+    parsed.callback = update.callback_query
+    return parsed
+  }
+
+  if (update.poll_answer) {
+    parsed.type = "poll_answer"
+    parsed.poll = update.poll_answer
+    return parsed
+  }
+
+  if (update.edited_message) {
+    parsed.type = "edited_message"
+    parsed.message = update.edited_message
+    return parsed
+  }
+
+  return parsed
 }
 
 module.exports = parseUpdate
